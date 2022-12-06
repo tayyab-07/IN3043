e0ed58be4bb26e75afa76c325206d22ed9c8fe03
tayyab-07 <tayyabhussain2001@outlook.com>
2022-12-06 02:21:12 +0000

    order final

--- Orders.hs
+++ Orders.hs
@@ -1,8 +1,8 @@
 module Orders where
 
 import Data.List
-import Data.Map (Map)
-import qualified Data.Map as Map
+import Data.Map 
+import Data.Maybe
 
 type Customer = String
 type Product = String
@@ -15,34 +15,111 @@ data Order = Order Customer Product Double
 data Delivery = Delivery Product Double
     deriving (Show)
 
-frequencyMap :: Ord a => [a] -> Map a Int
-frequencyMap xs = Map.unionsWith (+) [Map.singleton x 1 | x <- xs]
+-- NOTE --
+-- We didn`t know if by the statement in the coursework "You may assume that the quantity is greater than zero" 
+-- meant that the the quantities of products would be greater than 0 or
+-- if you meant that the list of Orders and list of Deliveries would be greater than 0 (not an empty list)
+-- With this in mind, we decided, just in case, to add lines for each function that look like:
+-- "numProducts [] = []" which prevent the solution from crashing if there is an empty list
+--  
 
-frequency :: [Char] -> [(Char, Int)]
-frequency ws = [(head g, length g) | g <- group (sort ws)]
+-- ----- QUESTION 1 -----
+-- All customers who submitted an order, with the number of different products each of them ordered.
+--
+-- STRATEGY USED: --
+-- Find all unique products in [Order] 
+-- Remove duplicates from that list using "nub"
+-- Use "sum" to add all unique products and stores the value in "num"
+--
+uniqueProducts :: [Order] -> [(Customer,Product)]
+uniqueProducts orders = 
+   [ (c, p) | (c, p) <- (nub [ (c, p) | (Order c p q) <- orders])]
 
--- All customers who submitted an order, with the number of different
--- products each of them ordered.
 numProducts :: [Order] -> [(Customer, Int)]
-numProducts [(Order c p n)] = [(head o, length o) | o <- group (sort [c])]
+numProducts [] = []
+numProducts orders = 
+   nub [(c, num) | (c, _) <- uniqueProducts orders, 
+   let 
+   num = 
+      sum [1 | (c1, p1) <- uniqueProducts orders, 
+      c == c1] ]
 
+
+-- ----- QUESTION 2 -----
 -- All products that have been ordered, with the total quantity of each.
-productQuantities :: [Order] -> [(Product, Double)]
-productQuantities [(Order c p n)] = undefined
+--
+-- STATEGY USED: --
+-- Function to map all product quantities to the second element in a tuple
+-- e.g. Chair, 3, 5, 8, 2...
+-- Function using "sum" to add quantities of mapped products found using Map.Lookup
+-- "nub" to remove dupliactes and then "sum" to get final summed quantities from the list
+--
+mapProductQuantity :: [(Product, Double)] -> Data.Map.Map Product [Double]  
+mapProductQuantity mpq =  
+   Data.Map.fromListWith (++) $ Data.List.map (\(a,b) -> (a,[b])) mpq
 
-productQuantities1 :: Ord [orders] => [Order] -> Map orders Int 
-productQuantities1 [(Order c p n)] = Map.unionsWith (+) [Map.singleton o 1 | o <- [p]]
+calculateSum :: Product -> [(Product, Double)] -> Double
+calculateSum p pQuantity = 
+   sum $ fromJust (Data.Map.lookup p $ mapProductQuantity pQuantity)
 
+productQuantities :: [Order] ->  [(Product, Double)]
+productQuantities [] = []
+productQuantities orders = 
+   nub [ (p, q1) | (Order c p q) <- (orders), 
+   let 
+   q1 = 
+      sum ( [ calculateSum p1 [ (p1, q1) ] 
+      | (Order c1 p1 q1) <- (orders), 
+      p == p1]) ]
+
+
+-- ----- QUESTION 3 -----
 -- The customers and products for which the customer has ordered
 -- more than half the total quantity for that product.
-majority :: [Order] -> [(Customer, Product)]
-majority orders = undefined
+--
+-- STRATEGY USED: -- 
+-- Used ProductQuantities to get total quantity
+-- Compared total quantities between customers to find which customer had more than half
+--
+majority :: [Order] -> [(Customer,Product)]
+majority [] = []
+majority orders = 
+   [ (c, p) | (Order c p q) <-  orders, 
+   (pQuantity, q1)  <-  (productQuantities orders), 
+   p == pQuantity && q > (q1/2) ]
 
+
+-- ----- QUESTION 4 -----
 -- Products for which the total quantity ordered exceeds the
 -- total quantity delivered, with the difference in quantity.
-shortfall :: [Order] -> [Delivery] -> [(Product, Double)]
-shortfall orders deliveries = undefined
+--
+-- STRATEGY USED: --
+-- Fucntion that does the same thing as ProductQuantities but for deliveries
+-- If the product is the same and ordered quantity is higher than delivery
+-- Then do order quantity minus delivery quantity
+--
+deliveryQuantity :: [Delivery] ->  [(Product, Double)]
+deliveryQuantity [] = []
+deliveryQuantity deliveries = 
+   nub [ (p, q1) | (Delivery p q) <- deliveries,  
+   let 
+   q1 = 
+      sum ( [ calculateSum p1 [ (p1, q1) ]
+      | (Delivery p1 q1) <- deliveries,
+      p == p1 ] ) ]
 
+shortfall :: [Order] -> [Delivery] -> [(Product, Double)]
+shortfall [] [] = []
+shortfall orders deliveries = 
+   [ (p1, difference) | (p1, q1) <- productQuantities orders, 
+   (p2, q2) <- deliveryQuantity deliveries, 
+   p1 == p2 && q1 > q2, 
+   let 
+   difference = 
+      q1 - q2 ]
+
+
+-- ----- QUESTION 5 -----
 -- Allocation of quantities of products to customers.
 --
 -- No customer should be allocated more of a given product than they have
@@ -53,5 +130,26 @@ shortfall orders deliveries = undefined
 -- For example, if the delivered quantity of a product is half the the
 -- total ordered quantity of that product, each customer would receive
 -- half of what they ordered.
+--
+-- STRATEGY USED: --
+-- Function to be able to divide floating point numbers without losing precision
+-- Get total ordered quantity
+-- Get total delivery quantity
+-- If delivery is less then ordered
+-- Then divide using function to work out how much each customer should receive
+-- 
+precision :: Double -> Int -> Double
+precision d n = 
+   fromInteger (round (d * (10^n))) / (10^n)
+
 share :: [Order] -> [Delivery] -> [(Customer, Product, Double)]
-share orders deliveries = undefined
+share [] [] = []
+share orders deliveries = 
+   [ variableOrderedQuantity | (Order c p q) <- orders, 
+   (p1, q1) <- productQuantities orders, 
+   (p2, q2) <- deliveryQuantity deliveries, 
+   p == p1,  
+   p == p2, 
+   let 
+   variableOrderedQuantity = 
+      if (q1 - q2) >= 0   then ( c, p, (precision (q2/q1*q) (2)) )   else (c, p, q) ]
