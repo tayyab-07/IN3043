9cc66265eb7adfb8a302b48cd1d8773e8bfb9dc3
tayyab-07 <tayyabhussain2001@outlook.com>
2023-01-15 06:37:58 +0000

    Week8 complete

--- Week8/Week8.hs
+++ Week8/Week8.hs
@@ -36,21 +36,63 @@ foldLTree f (Branch x y) = f (foldLTree f x) (foldLTree f y)
 sumLTree2 :: Num a => LTree a -> a
 sumLTree2 = foldLTree (+)
 
+mapLTree :: (a -> b) -> LTree a -> LTree b
+mapLTree f (Leaf x) = Leaf (f x)
+mapLTree f (Branch l r) = Branch (mapLTree f l) (mapLTree f r)
 
+size :: LTree a -> Int
+size = sumLTree2 . mapLTree (\x -> 1)
 
+depthMap :: LTree a -> Int
+depthMap = foldLTree (\ n m -> max n m + 1) . mapLTree (\x -> 0)
 
+listLeavesMap :: LTree a -> [a]
+listLeavesMap = foldLTree (++) . mapLTree (\x -> [x])
 
+mirrorMap :: LTree a -> LTree a
+mirrorMap = foldLTree (flip Branch) . mapLTree Leaf
 
+data Element = Element Name [Attribute] [Content]
+type Name = String
+type Attribute = (Name, String)
+data Content = Text String | Child Element
 
+printElement :: Element -> String
+printElement (Element n attrs []) = "<" ++ unwords (n : map printAttr attrs) ++ "/>"
+printElement (Element n attrs body) = "<" ++ unwords (n : map printAttr attrs) ++ ">" ++
+concat (map printContent body) ++ "</" ++ n ++ ">"
 
+printContent :: Content -> String
+printContent (Text s) = encodeString s
+printContent (Child e) = printElement e
 
+printAttr :: Attribute -> String
+printAttr (n, val) = n ++ "=\"" ++ encodeString val ++ "\""
 
+data Prop a
+   = Var a
+   | Not (Prop a)
+   | And (Prop a) (Prop a)
+   | Imply (Prop a) (Prop a)
+   | Or (Prop a) (Prop a)
+   | Equiv (Prop a) (Prop a)
+   deriving (Show)
 
+mapProp :: (a -> b) -> Prop a -> Prop b
+mapProp f (Var v) = Var (f v)
+mapProp f (Not p) = Not (mapProp f p)
+mapProp f (And p q) = And (mapProp f p) (mapProp f q)
+mapProp f (Imply p q) = Imply (mapProp f p) (mapProp f q)
+mapProp f (Or p q) = Or (mapProp f p) (mapProp f q)
+mapProp f (Equiv p q) = Equiv (mapProp f p) (mapProp f q)
 
-
-
-
-
+evalBool :: Prop Bool -> Bool
+evalBool (Var b) = b
+evalBool (Not p) = not (evalBool p)
+evalBool (And p q) = evalBool p && evalBool q
+evalBool (Imply p q) = not (evalBool p) || evalBool q
+evalBool (Or p q) = evalBool p || evalBool q
+evalBool (Equiv p q) = evalBool p == evalBool q
 
 
 
